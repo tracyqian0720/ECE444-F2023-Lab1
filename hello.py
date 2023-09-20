083da42ac0b6fd7260f8dfeac020a221e51ecf19
tracy <tracy.qian@mail.utoronto.ca>
2023-09-19 20:43:55 -0400

    added 2-1 2-2

--- hello.py
+++ hello.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!</h1>"
 
+
 @app.route("/user/<name>")
-def user(name)
-    return "<h1> Hello, {}</h1>".format(name)
-    
+def user(name):
+    return "<h1> Hello,{}!</h1>".format(name)
+
