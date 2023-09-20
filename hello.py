699daf4735523b72a68cb510963a42d15ff4b571
tracy <tracy.qian@mail.utoronto.ca>
2023-09-20 15:30:47 -0400

    activity 3 completed

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
-@app.route("/")
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.route('/')
 def index():
     return "<h1>Hello World!</h1>"
 
-
-@app.route("/user/<name>")
+@app.route('/user/<name>')
 def user(name):
-    return "<h1> Hello,{}!</h1>".format(name)
+    return render_template('user.html', name=name,  current_time=datetime.utcnow())
 
