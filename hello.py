3b77acf94188d4b2cf7f60d7940a3bae36adf7a3
tracy <tracy.qian@mail.utoronto.ca>
2023-09-20 15:35:31 -0400

    updated activity 3 - added error pages

--- hello.py
+++ hello.py
@@ -16,3 +16,11 @@ def index():
 def user(name):
     return render_template('user.html', name=name,  current_time=datetime.utcnow())
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
