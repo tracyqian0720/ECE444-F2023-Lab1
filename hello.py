9e7d6ab1b56ecd37d249671aacf414ac69f647c2
tracy <tracy.qian@mail.utoronto.ca>
2023-09-20 20:27:01 -0400

    activity 4 completed

--- hello.py
+++ hello.py
@@ -1,21 +1,48 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField,EmailField
+from wtforms.validators import DataRequired,Email
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config['SECRET_KEY'] = 'ece44'
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>Hello World!</h1>"
+    form = NameForm()
+
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name,  current_time=datetime.utcnow())
 
+"""
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -24,3 +51,4 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+"""
